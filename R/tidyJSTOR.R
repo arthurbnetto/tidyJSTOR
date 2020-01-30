6644c5be502bd55300da43c76fe02941e12454c7
arthurbnetto <51966697+arthurbnetto@users.noreply.github.com>
2020-01-30 17:09:17 -0500

    Update tidyJSTOR.R

--- R/tidyJSTOR.R
+++ R/tidyJSTOR.R
@@ -125,9 +125,9 @@ plotCleaningResult <- function(dfClean, dfDirt)
   	ggplot2::theme(panel.grid.minor = element_blank(), panel.border = element_rect(linetype = "solid", color = "grey", fill = NA), 
 		legend.position="bottom", legend.direction="horizontal", legend.title = element_blank(),
 		#strip.text.x = element_text(size = 12, colour = "black"),
-		axis.text.y = element_text(size=14),
-		axis.text.x = element_text(angle = 60, hjust = 1, size=14),
-		text=element_text(family="serif")) 
+		axis.text.y = ggplot2::element_text(size=14),
+		axis.text.x = ggplot2::element_text(angle = 60, hjust = 1, size=14),
+		text=ggplot2::element_text(family="serif")) 
 }
 
 ##############################################
